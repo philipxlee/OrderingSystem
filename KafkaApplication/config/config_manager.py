4d41b9b511d35390ec288c8b0c95dfbc0bc4e245
Philip Lee <philipleektx@gmail.com>
2024-02-21 13:41:07 -0500

    Created config with confluent

---
+++ KafkaApplication/config/config_manager.py
@@ -0,0 +1,27 @@
+class read_config():
+
+    """
+    Reads the client configuration from client.properties and returns it as a key-value map
+    """
+    def __init__(self):
+        self.config = self.read_config()
+
+    def read_config(self):
+        config = {}
+        with open("client.properties") as fh:
+            for line in fh:
+                line = line.strip()
+                if len(line) != 0 and line[0] != "#":
+                    parameter, value = line.strip().split('=', 1)
+                    config[parameter] = value.strip()
+        return config
+    
+    def get_config(self):
+        return self.config
+    
+
+
+
+
+
+
